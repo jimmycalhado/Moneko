ada22d6276c5c5ba4a9a9a159f7455e2868fe9bf
MORIM302 <gfamorim2@gmail.com>
2025-05-27 16:56:04 -0300

    befly

--- Scripts/inimigos/Befly/befly.gd
+++ Scripts/inimigos/Befly/befly.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+<<<<<<< Updated upstream
 @export var speed: float = 100.0 
 @export var direction: Vector2 = Vector2.LEFT  
 
@@ -26,3 +27,24 @@ func _on_area_2d_body_entered(body):
 
 func _on_area_2d_body_exited(body):
 	moving = false
+=======
+@export var speed: float = 250.0
+@export var gravity: float = 980.0
+
+var direction: Vector2 = Vector2.LEFT
+var activated: bool = false
+
+func _physics_process(delta):
+	if activated:
+		velocity.x = direction.normalized().x * speed
+	else:
+		velocity.x = 0
+
+
+	move_and_slide()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		activated = true
+		direction = (body.global_position - global_position).normalized()
+>>>>>>> Stashed changes
