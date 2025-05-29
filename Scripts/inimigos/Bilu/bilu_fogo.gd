f440118fff439d2a6fd3dd28aa8c2b46559eee07
jimmycalhado <jimmyandrey.1208@gmail.com>
2025-05-28 23:16:20 -0300

    fase 2 completa

--- Scripts/inimigos/Bilu/bilu_fogo.gd
+++ Scripts/inimigos/Bilu/bilu_fogo.gd
@@ -6,45 +6,39 @@ const jump = -400.0
 
 @onready var animation: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D =  $bilu
+@onready var ground_detector: RayCast2D =  $ground_detector
+@onready var player_detector: RayCast2D =  $player_detector2
+@onready var spawn_fireball: Marker2D =  $fireball_spawn
 
 var running: bool = false
 var player = null
-var direction = 0
+var direction = 1
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
-	$detectaplayer.body_entered.connect(on_body_entered)
 	animation
 	
 func _physics_process(delta: float) -> void:
-	# Gravidade
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	
+	if is_on_wall():
+		direction *= -1
+		sprite.scale.x *= -1
+		player_detector.scale.x *= -1
+
+	if not ground_detector.is_colliding():
+		direction *= -1
+		sprite.scale.x *= -1
+		player_detector.scale.x *= -1
+		
+	velocity.x = speed * direction * 100
+	velocity.y += gravity * delta
 		 
 	move_and_slide()	
 	_animation()
-	rotate_sprite()
 	return
 	
 func _animation():
 	if velocity.x != 0 :
-		pass
+		animation.play("run")
 	else:
 		animation.play("idle")
-
-	rotate_sprite()
-
-func rotate_sprite() -> void:
-	# Girar sprite
-	if velocity.x < 0:
-		sprite.flip_h = true
-	elif velocity.x > 0:
-		sprite.flip_h = false
-
-func on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		player = body
-	return
-	
-func _on_area_2d_body_exited(body):
-	player = null
